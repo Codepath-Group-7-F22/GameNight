de173be75327e9efe7093ef070c5e642793b6212
dev-lester94 <dev.lester94@gmail.com>
2022-10-28 20:22:00 -0500

    uidone

---
+++ GameNight/GameNight/RegistrationViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RegistrationViewController.swift
+//  GameNight
+//
+//  Created by lester on 10/24/22.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //scrollView.isUserInteractionEnabled = true
+        //scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: scrollView.bounds.height * 4)
+
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+//        scrollView.isUserInteractionEnabled = true
+        //scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: scrollView.bounds.height * 4)
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
